fced2d280da51f252588272b82d449720afb24e4
ttcksf <ttc0104ksf1993@yahoo.co.jp>
2024-05-02 22:04:21 +0900

    add:db-pdo-advance

--- db-pdo-advance/index.php
+++ db-pdo-advance/index.php
@@ -1,20 +1,19 @@
 <?php
 try{
   $user = "php-demo";
-  // エラーを発生させる
-  // $pass = "php1234";
-  $pass = "php123";
-  
+  $pass = "php1234";
+  // PDOクラスをインスタンス化してDB操作ができるようにする
   $dbh = new PDO("mysql:host=localhost;dbname=php-demo;charset=utf8", $user, $pass);
   $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  // SQL文の準備
   $sql = "SELECT * FROM users";
+  // SQL文の実行（queryメソッドの引数にSQL文を入れると実行される）
   $stmt = $dbh->query($sql);
+  // SQL文の実行による結果を取得する（fetchAllにすると配列になる）
   $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
   print_r($result);
   $dbh = null;
 }catch(Exception $e){
-  // 例外クラスを変数$eに代入して参照できるようにする
-  // $eからメッセージを取得して表示する
   echo "エラーが発生しました:" . htmlspecialchars($e->getMessage());
   die();
 }
