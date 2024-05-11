eb46a1515ca379add19e49c8f26363f1681c73f6
ttcksf <ttc0104ksf1993@yahoo.co.jp>
2024-05-12 03:35:51 +0900

    add:number-basic

---
+++ number-basic/index.php
@@ -0,0 +1,34 @@
+<?php
+  $num1 = 10;
+  $num2 = 20;
+  // echo $num1 * $num2;
+
+  // echo 2 * (4 + 5);
+
+  // $num1++;
+  // これと同じ
+  // $num1 = $num + 1;
+  // echo $num1;
+
+  // $num2 += 10;
+  // これと同じ（1以外の足し算に使う）
+  // $num2 = $num2 + 10;
+  // echo $num2;
+
+  // 小数点の丸め込み
+  $test3 = 2.14;
+  // 切り捨て
+  // echo floor($test3);
+  // 切り上げ
+  echo ceil($test3);
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+</body>
+</html>
