da9830cc1667733e3b8e4305a2572a4ebe254a03
ttcksf <ttc0104ksf1993@yahoo.co.jp>
2024-05-13 14:29:29 +0900

    add:while-loop-basic

--- while-loop-basic/index.php
+++ while-loop-basic/index.php
@@ -1,5 +1,40 @@
 <?php
-  
+    $arr = ["aaa","bbb","ccc"];
+    // インデックス番号を追いかける
+    $index = 0;
+    while($index < count($arr)){
+      // echo $index;
+      echo $arr[$index];
+      echo "<br/>";
+      $index++;
+    }
+    
+      // 多重配列の時はインデックス番号を追いかけながら、
+  // 中身の要素が連想配列になっているのでキー名を指定する
+    $users = [
+      [
+        "name" => "yamada",
+        "age" => 20,
+      ],
+      [
+        "name" => "tanaka",
+        "age" => 21,
+      ],
+      [
+        "name" => "yoshida",
+        "age" => 22,
+      ],
+    ];
+
+    $index = 0;
+    while($index < count($users)){
+      echo $index;
+      echo $users[$index]["name"];
+      echo "<br/>";
+      $index++;
+    }
+
+
 ?>
 <!-- そのためHTMLの範囲外に書いたとしても動く -->
 <!DOCTYPE html>
