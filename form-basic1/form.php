b289574eee294a39f5940a9330df3f0de8cc320a
ttcksf <ttc0104ksf1993@yahoo.co.jp>
2024-05-21 13:18:30 +0900

    add:form-basic1

---
+++ form-basic1/form.php
@@ -0,0 +1,32 @@
+<?php
+  // name属性の値が配列になって$_GETというグローバル変数に格納される仕組み
+  // 入力値、クリックがないときはデータはないものとして配列になっている
+  // フォームとしてはページのリロードのたびに表示されたくない
+  // print_r($_GET);
+
+  // name="submit"に値がある時（ボタンを押した時）にデータ取得する
+  // データが空の場合は取得作業を実行しないことになる
+  if(isset($_GET["submit"])){
+    print_r($_GET);
+  }
+
+?>
+
+
+<?php include("./partials/header.php");?>
+
+<!-- getメソッドはアドレスバーにデータが表示される -->
+<!-- formタグのactionには受け取るファイルを指定するがgetメソッドは受け取るものがないため自分自身を指定することになる -->
+<form action="form.php" method="GET">
+<label for="">
+    氏名
+    <input type="text" name="name">
+  </label>
+  <label for="">
+    Email
+    <input type="text" name="email">
+  </label>
+  <input type="submit" name="submit" value="送信">
+</form>
+
+<?php include("./partials/footer.php");?>
