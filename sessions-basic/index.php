a4074393330542ebdb1367bbedc74c24f9f244bc
ttcksf <ttc0104ksf1993@yahoo.co.jp>
2024-06-02 01:00:46 +0900

    add:session-basic

---
+++ sessions-basic/index.php
@@ -0,0 +1,26 @@
+<?php
+  if(isset($_POST["submit"])){
+    session_start();
+
+    $_SESSION["name"] = $_POST["name"];
+
+    echo $_SESSION["name"];
+  }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <form action="<?php echo $_SERVER["PHP_SELF"];?>" method="POST">
+    <input type="text" name="name"/>
+    <input type="submit" name="submit" value="送信" />
+  </form>
+</body>
+</html>
+
+
